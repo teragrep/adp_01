0e9c68fb92931ec5164e6279c6a252e92676a0c7
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-08-12 09:57:28 +0300

    Make the dedup.py be more logical

--- helm/archive-datagenerator-static/config/init-extras/dedup.py
+++ helm/archive-datagenerator-static/config/init-extras/dedup.py
@@ -7,16 +7,16 @@ host="dedup.{{.Values.ipa.domain}}"
 tag="dedup"
 # Starting timestamp
 starting_epoch = 1696464000 # Your time zone: Thursday, October 5, 2023 3:00:00 GMT+03:00 DST
-for message in range(1,4):
-    for hour in range(1,4):
-        # Bump timestamp by n hours
-        new_epoch = starting_epoch + hour*3600
-        # Get epoch hour, floored
-        epoch_hour = new_epoch - new_epoch%3600
-        timestamp = datetime.datetime.fromtimestamp(new_epoch, timezone)
-        dirname=f"{basepath}/{host}/{timestamp.year}/{str(timestamp.month).zfill(2)}/{str(timestamp.day).zfill(2)}"
-        filename=f"{dirname}/{tag}-@{epoch_hour}-{timestamp.year}{str(timestamp.month).zfill(2)}{str(timestamp.day).zfill(2)}{str(timestamp.hour).zfill(2)}.log"
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        with open(filename, "a+") as fh:
-            fh.write(f"<134>1 {timestamp.isoformat('T')} {host} {tag} - - - This duplicated event {host}-{tag} number #{message}\n")
+for hour in range(1,4):
+    # Bump timestamp by n hours
+    new_epoch = starting_epoch + hour*3600
+    # Get epoch hour, floored
+    epoch_hour = new_epoch - new_epoch%3600
+    timestamp = datetime.datetime.fromtimestamp(new_epoch, timezone)
+    dirname=f"{basepath}/{host}/{timestamp.year}/{str(timestamp.month).zfill(2)}/{str(timestamp.day).zfill(2)}"
+    filename=f"{dirname}/{tag}-@{epoch_hour}-{timestamp.year}{str(timestamp.month).zfill(2)}{str(timestamp.day).zfill(2)}{str(timestamp.hour).zfill(2)}.log"
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    with open(filename, "a+") as fh:
+        for ignored in range(1,4):
+            fh.write(f"<134>1 {timestamp.isoformat('T')} {host} {tag} - - - This duplicated event #{hour}\n")
